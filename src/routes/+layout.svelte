cc7b8f9515e629c596a566077167854b500450a6
Suyashtnt <suyashtnt@gmail.com>
2023-10-02 16:26:12 +0200

    feat: basic layout and todo app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+</svelte:head>
+
+<header>
+    <hgroup class="text-center">
+        <h1>Doen</h1>
+        <h2>Your next todo list</h2>
+    </hgroup>
+</header>
+
+<main class="mx-4">
+    <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,106 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import { flip } from 'svelte/animate'
+
+    interface Todo {
+        done: boolean
+        id: string
+        text: string
+    }
+
+    let todos: Todo[] = []
+
+    const addTodo = (text: string) => {
+        todos = [
+            ...todos,
+            {
+                done: false,
+                id: createUniqueId(),
+                text,
+            },
+        ]
+    }
+
+    const removeTodo = (id: string) => {
+        todos = todos.filter(todo => todo.id !== id)
+    }
+
+    const createUniqueId = () => {
+        let id = window.crypto.randomUUID()
+
+        const todoExists = todos.find(todo => todo.id === id)
+        if (todoExists) {
+            id = createUniqueId()
+        }
+
+        return id
+    }
+
+    if (browser) {
+        const rawTodos = localStorage.getItem('todos')
+
+        if (rawTodos) {
+            todos = JSON.parse(rawTodos)
+        }
+    }
+
+    $: if (browser) {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }
+
+    let newTodo = ''
+
+    let showDone = true
+
+    $: todoList = showDone ? todos : todos.filter(todo => !todo.done)
+</script>
+
+<div class="flex flex-row gap-4 mb-4">
+    <h3 class="my-auto">Todos</h3>
+    <div class="flex gap-4 items-center">
+        <label for="show-done">Show done</label>
+        <input
+            type="checkbox"
+            bind:checked={showDone}
+            id="show-done"
+        >
+    </div>
+</div>
+
+<ul class="flex flex-col">
+    {#each todoList as todo (todo.id)}
+        <li class="grid grid-cols-6 gap-4" animate:flip={{ duration: 150 }}>
+            <input
+                type="text"
+                bind:value={todo.text}
+                class="col-span-4 {todo.done ? 'bg-green-500/40' : ''}"
+            >
+
+            <button
+                on:click={() => todo.done = !todo.done}
+                class:bg-red-500={todo.done}
+                class:bg-blue-500={!todo.done}
+            >
+                {#if todo.done}
+                    Unmark
+                {:else}
+                    Mark Done
+                {/if}
+            </button>
+            <button on:click={() => removeTodo(todo.id)}>Remove</button>
+        </li>
+    {/each}
+</ul>
+
+<form
+    on:submit|preventDefault={() => addTodo(newTodo)}
+    class="grid grid-cols-7 gap-4"
+>
+    <input
+        type="text"
+        bind:value={newTodo}
+        placeholder="What needs to be done?"
+        class="col-span-6"
+    >
+    <button type="submit">Add</button>
+</form>
